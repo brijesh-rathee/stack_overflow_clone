71638f207e72fccea533709e7ea63747f59e8ee8
jharshavardhan2003 <harshavardhandec11@gmail.com>
2025-07-18 18:32:17 +0530

    performed the crud operations on questions and comments

--- src/main/java/com/stackclone/stackoverflow_clone/controller/QuestionController.java
+++ src/main/java/com/stackclone/stackoverflow_clone/controller/QuestionController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@RequestMapping("/question")
+@RequestMapping("/questions")
 @RequiredArgsConstructor
 public class QuestionController {
 
