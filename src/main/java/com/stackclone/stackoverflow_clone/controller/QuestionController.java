8c766825eaae4067757e6e4104c5d45c375dc226
Brijesh rathee <brijeshrathee2@gmail.com>
2025-07-18 16:11:40 +0530

    files created

---
+++ src/main/java/com/stackclone/stackoverflow_clone/controller/QuestionController.java
@@ -0,0 +1,7 @@
+package com.stackclone.stackoverflow_clone.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class QuestionController {
+}
---
+++ src/main/java/com/stackclone/stackoverflow_clone/repository/QuestionRepository.java
@@ -0,0 +1,9 @@
+package com.stackclone.stackoverflow_clone.repository;
+
+import com.stackclone.stackoverflow_clone.entity.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuestionRepository extends JpaRepository<Question, Integer> {
+}
